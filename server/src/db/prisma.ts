ca0c1294cb452789e9271ca785e4152c8dc582de
JuriRappold <juri@rappold.berlin>
2026-05-05 16:38:03 +0200

    added CRUD-query files, bundled in query.ts; Implemented reading of the currents week schedule

--- server/src/db/prisma.ts
+++ server/src/db/prisma.ts
@@ -4,7 +4,6 @@ import { PrismaClient } from "../generated/prisma/client.js";//shouldn't it be .
 
 const { Pool } = pg;
 const connectionString = process.env.DATABASE_URL;
-
 const pool = new Pool({ connectionString });
 const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
---
+++ server/src/db/queries.ts
@@ -0,0 +1,19 @@
+/*
+--> have classes for each CRUD operation?
+ */
+
+
+export * from './createQueries.js'
+export * from './readQueries.js'
+export * from './updateQueries.js'
+export * from './deleteQueries.js'
+
+
+
+
+
+
+
+
+
+
